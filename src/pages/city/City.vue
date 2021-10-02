186954300a7a77a07741acdb407a1bcceee67849
aonly <atiffiany@outlook.com>
2021-10-02 10:06:57 +0800

    feat:city-search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,13 +1,17 @@
 <template>
-  <div><city-header></city-header></div>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 <script>
 import CityHeader from '@/pages/city/components/Header.vue'
-
+import CitySearch from '@/pages/city/components/Search.vue'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
