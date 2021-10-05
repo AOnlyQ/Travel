206dc3cff48068c39132bb5ee75e9aee5a49cb16
aonly <atiffiany@outlook.com>
2021-10-05 09:58:56 +0800

    feat:banner finish

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import List from '@/pages/list/List'
 import City from '@/pages/city/City.vue'
+import Detail from '@/pages/detail/Detail.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -16,6 +17,11 @@ export default new Router({
       path: '/city',
       name: 'City',
       component: City
+    },
+    {
+      path: '/detail/:id',
+      name: 'Detail',
+      component: Detail
     },
     {
       path: '/list',
